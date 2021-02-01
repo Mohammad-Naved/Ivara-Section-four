1468530220f4e836a1129636b733c6aa9c6a5d29
Mohammad-Naved <74970362+Mohammad-Naved@users.noreply.github.com>
2021-02-01 15:23:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './component/FirstPart.css'
 import FourthSection from './component/FourthSection';
 function App() {
   return (
